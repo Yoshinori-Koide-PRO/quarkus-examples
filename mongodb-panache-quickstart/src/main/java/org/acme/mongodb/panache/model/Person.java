3b4a72f851beb0ae33f007a826dd51553fe411a0
Yoshinori-Koide-PRO <roomtv@pro-japan.co.jp>
2020-01-29 18:37:39 +0900

    Person/nameでのコレクションの動的な切り替えに対応

--- mongodb-panache-quickstart/src/main/java/org/acme/mongodb/panache/model/Person.java
+++ mongodb-panache-quickstart/src/main/java/org/acme/mongodb/panache/model/Person.java
@@ -1,22 +1,72 @@
 package org.acme.mongodb.panache.model;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.time.LocalDate;
-
+import java.util.Map;
+import javax.json.bind.annotation.JsonbDateFormat;
 import org.bson.codecs.pojo.annotations.BsonProperty;
 
 import io.quarkus.mongodb.panache.MongoEntity;
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
 
-@MongoEntity(collection="ThePerson")
+@MongoEntity(collection = "ThePerson")
 public class Person extends PanacheMongoEntity {
     public enum Status {
         Alive, DECEASED
     }
+
     public String name;
 
     // will be persisted as a 'birth' field in MongoDB
     @BsonProperty("birth")
+    @JsonbDateFormat("yyyy-MM-dd")
     public LocalDate birthDate;
 
     public Status status;
+
+    public void setName(String name) {
+        this.name = name;
+
+        Annotation mongoEntity = this.getClass().<MongoEntity>getAnnotation(MongoEntity.class);
+
+        changeAnnotationValue(mongoEntity, "collection", "ThePerson/" + name);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void changeAnnotationValue(Annotation annotation, String key, final String newValue) {
+
+        try {
+            Field field = Class.class.getDeclaredField("annotationData");
+            field.setAccessible(true);
+            Object annotationData = field.get(Person.class);
+            Field annotationsField = annotationData.getClass().getDeclaredField("annotations");
+            annotationsField.setAccessible(true);
+            Map<Class<? extends Annotation>, Annotation> annotations =
+                    (Map<Class<? extends Annotation>, Annotation>) annotationsField
+                            .get(annotationData);
+            Annotation newMongoEntity = new MongoEntity() {
+                @Override
+                public Class<? extends Annotation> annotationType() {
+                    return annotation.annotationType();
+                }
+
+                @Override
+                public String collection() {
+                    return newValue;
+                }
+
+                @Override
+                public String database() {
+                    return "";
+                }
+            };
+
+            annotations.put(MongoEntity.class, newMongoEntity);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
--- mongodb-panache-quickstart/src/test/java/org/acme/mongodb/panache/FruitResourceTest.java
+++ mongodb-panache-quickstart/src/test/java/org/acme/mongodb/panache/FruitResourceTest.java
@@ -1,21 +1,32 @@
 package org.acme.mongodb.panache;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import java.time.LocalDate;
+import org.acme.mongodb.panache.model.Person;
+import org.acme.mongodb.panache.model.Person.Status;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
 public class FruitResourceTest {
 
     @Test
     public void testHelloEndpoint() {
-        given()
-          .when().get("/fruits")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+
+        given().contentType(ContentType.JSON)
+                .body("{\"name\": \"Alice\", \"birthDate\":\"1980-12-31\",\"status\":\"Alive\"}")
+                .when().post("/person").then().statusCode(200).body(containsString("Alice"));
+    }
+
+    @Disabled
+    @Test
+    public void testPostPersonEndpoint() {
+        given().when().get("/person").then().statusCode(200).body(is("hello"));
     }
 
 }
